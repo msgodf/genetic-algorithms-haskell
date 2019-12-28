da4cf8e152e4fdae4ceeadf6a44b83eff594fd42
Mark Godfrey <msgodf@gmail.com>
2019-12-28 15:17:52 +0000

    Refactoring: extract function and tidy formatting.
    No functional differences, I'm just trying to improve readability.

--- src/Tree.hs
+++ src/Tree.hs
@@ -21,7 +21,7 @@ instance Ord Tree where
   (<=) a b = fitness a <= fitness b
 
 instance Genetic Tree where
-  fitness x = -(sum $ fmap (\(input, output) -> let (Leaf (Value v)) = evaluate (substitute x (Leaf (Value input))) in abs(output - v)) targets) - programLengthFitnessWeighting*(fromIntegral $ treeSize x)
+  fitness x = programFitnessOverInputs x targets
   mutate x g = subtreeMutation x g
   crossover (x,y) g = crossoverNodes (x,y) g
 
@@ -37,6 +37,13 @@ instance Random Tree where
   random g = (\(x, _, y) -> (x, y)) $ randomTree 0 g
   randomR _ g = random g
 
+programFitnessOverInputs :: Tree -> [(Double,Double)] -> Double
+programFitnessOverInputs x xs = -(sum $
+                                  fmap (\(input, output) -> let (Leaf (Value v)) = evaluate $
+                                                                  substitute x (Leaf (Value input)) in
+                                                              abs(output - v))
+                                  xs)
+                                - programLengthFitnessWeighting*(fromIntegral $ treeSize x)
 randomTree :: (RandomGen g) => Int -> g -> (Tree, Int, g)
 randomTree d g = if d >= maximumTreeDepth
   then
@@ -76,47 +83,55 @@ flipBranches (Leaf a) = Leaf a
 
 treeDepth :: Tree -> Int
 treeDepth t = f t 0 where
-  f = (\t d -> case t of (Branch o a b) -> if m > n then m else n where m = f a(d + 1)
-                                                                        n = f b (d + 1)
+  f = (\t d -> case t of (Branch o a b) -> if m > n
+                                           then m
+                                           else n where m = f a(d + 1)
+                                                        n = f b (d + 1)
                          (Leaf a) -> d)
 
 labelTree :: Tree -> Set Int
 labelTree t = f t 0 empty where
   f = (\t n xs -> case t of (Leaf a) -> (singleton n) `union` xs
-                            (Branch o a b) -> (singleton n) `union` (f a (2*n + 1) xs) `union` (f b (2*n + 2) xs))
+                            (Branch o a b) -> (singleton n) `union`
+                                              (f a (2*n + 1) xs) `union`
+                                              (f b (2*n + 2) xs))
 
 intersectionOfTreeLabels :: Tree -> Tree -> Set Int
 intersectionOfTreeLabels t1 t2 = (labelTree t2) `intersection` (labelTree t1)
 
 swapNodes :: (Tree, Tree) -> Int -> (Tree, Tree)
 swapNodes ts n = f ts n 0 where
-  f = (\ts n m -> case ts of (Branch o1 a b, Branch o2 c d) -> let (a2, c2) = f (a, c) (2*n + 1) m
-                                                                   (b2, d2) = f (b, d) (2*n + 2) m in
-                                                                 if n == m then (Branch o2 c2 d2, Branch o1 a2 b2)
-                                                                 else (Branch o1 a2 b2, Branch o2 c2 d2)
-                             (a, b) -> if n == m then (b, a) else (a, b))
+  f = (\ts n m -> case ts of
+          (Branch o1 a b, Branch o2 c d) -> let (a2, c2) = f (a, c) (2*n + 1) m
+                                                (b2, d2) = f (b, d) (2*n + 2) m in
+                                              if n == m
+                                              then (Branch o2 c2 d2, Branch o1 a2 b2)
+                                              else (Branch o1 a2 b2, Branch o2 c2 d2)
+          (a, b) -> if n == m then (b, a) else (a, b))
 
 replaceFirstLeafWithVariable :: Tree -> Tree
 replaceFirstLeafWithVariable t = fst $ f t where
-  f = (\t -> case t of (Leaf (Value t)) -> ((Leaf X), True)
-                       (Branch o a b) -> let (t1, r1) = f a
-                                             (t2, r2) = f b in
-                                           if r1
-                                           then (Branch o t1 b, r1)
-                                           else
-                                             if r2
-                                             then (Branch o a t2, r2)
-                                             else (Branch o a b, False)
-                       t -> (t, False))
+  f = (\t -> case t of
+          (Leaf (Value t)) -> ((Leaf X), True)
+          (Branch o a b) -> let (t1, r1) = f a
+                                (t2, r2) = f b in
+                              if r1
+                              then (Branch o t1 b, r1)
+                              else
+                                if r2
+                                then (Branch o a t2, r2)
+                                else (Branch o a b, False)
+          t -> (t, False))
 
 substituteNthNode :: Tree -> Tree -> Int -> Tree
 substituteNthNode t1 t2 n = f t1 t2 n 0 where
-  f = (\t1 t2 n m -> case t1 of (Leaf a) -> if n == m then t2 else (Leaf a)
-                                (Branch o a b) -> if n == m then t2
-                                                  else
-                                                    if m > n
-                                                    then Branch o a b
-                                                    else Branch o (f a t2 n (2*m + 1)) (f b t2 n (2*m + 2)))
+  f = (\t1 t2 n m -> case t1 of
+          (Leaf a) -> if n == m then t2 else (Leaf a)
+          (Branch o a b) -> if n == m then t2
+                            else
+                              if m > n
+                              then Branch o a b
+                              else Branch o (f a t2 n (2*m + 1)) (f b t2 n (2*m + 2)))
 
 subtreeMutation :: (RandomGen a) => Tree -> a -> (Tree, a)
 subtreeMutation t g = let labels = labelTree t
